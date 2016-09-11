378160fd90c2cd1d3295f07a2523034c8276f151
u174762 <sam.panga@hotmail.com>
2016-09-11 13:24:05 +0530

    Update Assignment3.java

--- ACD_JAVA_session3-assignment3/src/Assignment3.java
+++ ACD_JAVA_session3-assignment3/src/Assignment3.java
@@ -7,6 +7,18 @@ public class Assignment3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+                    int c;
+		    System.out.println("Please enter a number");
+		    
+		    int a =new Scanner(System.in).nextInt();
+		   
+		    Random t = new Random();
+		 
+		    // random integers in [0, 100]
+		 
+		    for (c = 1; c <= a-1; c++) {
+		      System.out.println(t.nextInt(c));
+		    }
 
 	}
 
